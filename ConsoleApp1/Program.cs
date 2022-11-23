c6667a2035e104ed739b48dd6cf557cdb75cef7c
Thinking^loper <96408793+JaeWonLee3003@users.noreply.github.com>
2022-11-23 10:23:02 +0900

    초기 커밋.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Cat
+{
+    public string Name = null;
+
+    // 속성은 반드시 초기화해야 하며 그렇지 않으면 경고 메세지가 출력 된다. 값이 없을 때는 null로 초기화 한다.
+    public Cat()
+    {
+        Console.WriteLine("생성자가 호출되었습니다.");
+
+    }
+}
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Cat myCat = new Cat();
+    }
+}
