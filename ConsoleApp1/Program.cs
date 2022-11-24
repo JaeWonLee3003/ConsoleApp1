e9706b9019ca1266b63a83f36a227be6ec35a3da
Thinking^loper <96408793+JaeWonLee3003@users.noreply.github.com>
2022-11-24 11:01:15 +0900

    중간이해

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,13 +1,53 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace Test
+namespace WpfApp1
 {
-   
-    class MainClass
+    public class LadderPosition
     {
+        public LadderPosition()
+        {
+        }
+
+        public LadderPosition(int x, int y)
+        {
+
+        }
+    }
+
+    public class StartBox
+    {
+        public int StartB = 0;
+    }
+    public class EndBox
+    {
+        public int EndB = 0;
+
+    }
+
+
+
+    public class mainClass
+    {
+        public static int StartB { get; private set; }
+        public static int EndB { get; private set; }
+
         public static void Main(string[] args)
         {
-           
+
+            Console.WriteLine("몇 개를 입력 하시겠습니까?");
+
+            int inputBoxNum = 0;
+
+            inputBoxNum = int.Parse(Console.ReadLine());
+
+            StartB = inputBoxNum;
+            Console.WriteLine("입력 받은 Start Box 의 개수는 \n" + StartB + " 개 입니다.");
+
+            LadderPosition LP = new LadderPosition();
         }
     }
 }
