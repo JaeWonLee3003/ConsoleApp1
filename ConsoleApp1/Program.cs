d8732ab87bfb9fdc9f5b6014b902af418386123d
Thinking^loper <96408793+JaeWonLee3003@users.noreply.github.com>
2022-11-28 09:42:18 +0900

    생성자들과 모두다 정리 완료 구동

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -26,6 +26,7 @@ namespace WpfApp1
             public int StartB = 0;
             public int inputBoxNum = 0; // 입력된 박스 개수
             public int StartInBox = 0; // 처음에 담길 박스 개수
+            public bool whileTrue = true; // 무조건 true를 하여 실행시킴
         }
 
         public class EndBox
@@ -37,16 +38,17 @@ namespace WpfApp1
 
     public class StartWhile
     {
-        private static bool whileTrue;
+        
 
 
         public static void Main(string[] args)
         {
+            StartBox StartMain = new StartBox();
+            EndBox EndMain = new EndBox();
 
-            while (whileTrue) // 바로 활성화
+            while (StartMain.whileTrue) // 바로 활성화
             {
-                StartBox StartMain = new StartBox();
-                EndBox EndMain = new EndBox();
+                
 
                 Console.WriteLine("몇 개를 입력 하시겠습니까?");
                 StartMain.inputBoxNum = int.Parse(Console.ReadLine()); // inputBoxNum 안에 횟수를 입력
@@ -61,28 +63,28 @@ namespace WpfApp1
                         EndMain.EndInBox = StartMain.StartB; // 마지막 들어갈 박스안에 할당
                         Console.WriteLine("입력할 텍스트 5개가 생성 되었습니다.\n");
                         Console.WriteLine("TEST : 생성 된 텍스트" + StartMain.StartInBox + "개 입니다. \n");
-                        whileTrue = false; // 강제종료 break; 가 안먹힘 이유는 모름
+                        StartMain.whileTrue = false; // 강제종료 break; 가 안먹힘 이유는 모름
                         break;
                     case 4:
                         StartMain.StartInBox = StartMain.StartB;
                         EndMain.EndInBox = StartMain.StartB; // 마지막 들어갈 박스안에 할당
                         Console.WriteLine("입력할 텍스트 4개가 생성 되었습니다.\n");
                         Console.WriteLine("TEST : 생성 된 텍스트" + StartMain.StartInBox + "개 입니다. \n");
-                        whileTrue = false;
+                        StartMain.whileTrue = false;
                         break;
                     case 3:
                         StartMain.StartInBox = StartMain.StartB;
                         EndMain.EndInBox = StartMain.StartB; // 마지막 들어갈 박스안에 할당
                         Console.WriteLine("입력할 텍스트 3개가 생성 되었습니다.\n");
                         Console.WriteLine("TEST : 생성 된 텍스트" + StartMain.StartInBox + "개 입니다. \n");
-                        whileTrue = false;
+                        StartMain.whileTrue = false;
                         break;
                     case 2:
                         StartMain.StartInBox = StartMain.StartB;
                         EndMain.EndInBox = StartMain.StartB; // 마지막 들어갈 박스안에 할당
                         Console.WriteLine("입력할 텍스트 2개가 생성 되었습니다.\n");
                         Console.WriteLine("TEST : 생성 된 텍스트" + StartMain.StartInBox + "개 입니다.\n ");
-                        whileTrue = false;
+                        StartMain.whileTrue = false;
                         break;
                     case 1:
                         StartMain.StartInBox = StartMain.StartB;
@@ -93,23 +95,25 @@ namespace WpfApp1
                         Console.WriteLine("다시 입력해주세요. ^^ ( 최대 5개 )\n");
                         break;
                 }
+
+                // Console.WriteLine("사다리에서 내려올 내용을 " + StartInBox + "개 적어주세요.\n");
+                string[] StartArr = new string[StartMain.StartInBox]; // StartArr 배열 안에 입력할 횟수인 StartInBox
+                Console.WriteLine("TEST : StartArr 배열 안에 들어간 길이는 " + StartArr.Length + "입니다.");
+                Console.WriteLine("입력 가능한 횟수는 :" + StartArr.Length + "회 입니다.");
+                for (int i = 0; i < StartArr.Length; i++)
+                {
+
+                    StartArr[i] = Console.ReadLine();
+
+                }
+
+                Console.WriteLine("입력 된 내용은 :" + StartArr[1]);
             }
 
-            StartBox Start = new StartBox();
-            EndBox End = new EndBox();
+            
 
-            // Console.WriteLine("사다리에서 내려올 내용을 " + StartInBox + "개 적어주세요.\n");
-            string[] StartArr = new string[Start.StartInBox]; // StartArr 배열 안에 입력할 횟수인 StartInBox
-            Console.WriteLine("TEST : StartArr 배열 안에 들어간 길이는 " + StartArr.Length + "입니다.");
-            Console.WriteLine("입력 가능한 횟수는 :" + StartArr.Length + "회 입니다.");
-            for (int i = 0; i < StartArr.Length; i++)
-            {
-
-                StartArr[i] = Console.ReadLine();
-
-            }
-
-            //Console.WriteLine("입력 된 내용은 :" + StartArr[i]);
+            
+            
         }
     }
 }
