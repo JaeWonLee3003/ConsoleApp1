9b90adfa94e87e0f977f3ddb528657f7daeea3ce
Thinking^loper <96408793+JaeWonLee3003@users.noreply.github.com>
2022-11-28 15:07:02 +0900

    할거 다했으니 이제 wpf 로 넘어감

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,10 +11,8 @@ namespace WpfApp1
 {
     public class LadderPosition
     {
-        public int Positionx = 0;
-        public int Positiony = 0;
-
-
+        //public int Positionx = 0;
+        //public int Positiony = 0;
         public void PositionDown()
         {
             Console.WriteLine("□↓□");
@@ -28,7 +26,6 @@ namespace WpfApp1
             Console.WriteLine("□←□");
         }
     }
-
     public class Ui_Class
     {
         public class Random
@@ -37,7 +34,6 @@ namespace WpfApp1
         }
         public class StartBox
         {
-
             public int StartB = 0;
             public int inputBoxNum = 0; // 입력된 박스 개수
             public int StartInBox = 0; // 처음에 담길 박스 개수
@@ -53,9 +49,6 @@ namespace WpfApp1
 
     public class StartWhile
     {
-
-
-
         public static void Main(string[] argsr)
         {
             StartBox StartMain = new StartBox();
@@ -63,8 +56,6 @@ namespace WpfApp1
 
             while (StartMain.whileTrue) // 바로 활성화
             {
-
-
                 Console.WriteLine("몇 개를 입력 하시겠습니까?");
                 StartMain.inputBoxNum = int.Parse(Console.ReadLine()); // inputBoxNum 안에 횟수를 입력
                 StartMain.StartB = StartMain.inputBoxNum; // StartB 변수 안에 횟수를 할당
@@ -72,7 +63,6 @@ namespace WpfApp1
 
                 switch (StartMain.StartB) // 입력 된 횟수를 케이스로 찾아감
                 {
-
                     case 5:
                         StartMain.StartInBox = StartMain.StartB; // 처음 들어갈 박스 박스안에 할당
                         EndMain.EndInBox = StartMain.StartB; // 마지막 들어갈 박스안에 할당
@@ -107,21 +97,13 @@ namespace WpfApp1
                         StartMain.whileTrue = false;
                         break;
                 }
-
                 LadderPosition LP = new LadderPosition(); // 사다리를 불러올 생성자 Ex ) : LP.PositionDown(); Result = > Console.WriteLine("↓");
-
                 // Console.WriteLine("사다리에서 내려올 내용을 " + StartInBox + "개 적어주세요.\n");
-
                 string[] StartArr = new string[StartMain.StartInBox]; // StartArr 배열 안에 입력할 횟수인 StartInBox
-
-
                 Console.WriteLine("시작 박스 입력 가능한 횟수는 :" + StartArr.Length + "회 입니다. \n");
-
                 for (int i = 0; i < StartArr.Length; i++)
                 {
-
                     StartArr[i] = Console.ReadLine();
-
                 }
                 string[] EndArr = new string[EndMain.EndInBox]; // EndArr 배열 안에 입력할 횟수인 EndInBox
 
@@ -129,46 +111,34 @@ namespace WpfApp1
 
                 for (int i = 0; i < EndArr.Length; i++)
                 {
-
                     EndArr[i] = Console.ReadLine();
-
                 }
-
                 StartWhile StartMain2 = new StartWhile();
-
                 while (StartMain.StartB >= 2) //StartB 가 2보다 많으면 실행 
                 {
                     Console.WriteLine("1 ) Yes \n2 ) No");
                     string Guess = Console.ReadLine();
                     int forStartNum = 0; //가
-                    int forEndNum = 0; //다
-                    int Plus = 1;
+                    int forEndNum = 0; //다                   
                     switch (int.Parse(Guess))
                     {
-
                         case 1:
                             Console.WriteLine("실행 되었습니다.\n");
                             for (int TryNumBox = 1; TryNumBox <= EndMain.EndInBox; TryNumBox++)
                             {
                                 Console.WriteLine("□" + TryNumBox + "번 결과 입니다.□");
-
                                 Console.WriteLine("□" + StartArr[forStartNum] + "□□□□□□□□"); // 가 - 나
                                 forStartNum++;
                                 LP.PositionDown();
-                                LP.PositionRight();
-                                LP.PositionDown();
-                                LP.PositionLeft();
+                                //LP.PositionRight();
+                                //LP.PositionDown();
                                 Console.WriteLine("□" + EndArr[forEndNum] + "□□□□□□□□");  // 다 - 라 
                                 forEndNum++; 
-
-                                // 출력 예상 결과 가 ↓ → ↓ ← 다
-                                // 출력 예상 결과 나 ↓ → ↓ ← 라
-
+                                // 출력 예상 결과 가 ↓  다
+                                // 출력 예상 결과 나 ↓  라
                             }
                             StartMain.StartB = 1;
                             break;
-
-
                         case 2:
                             Console.WriteLine("취소 되었습니다.");
                             StartMain.StartB = 1;
@@ -179,7 +149,3 @@ namespace WpfApp1
         }
     }
 }
-
-
-
-
